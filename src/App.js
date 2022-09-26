8c3a2403c37f518d4c5332d12f480f78ae416fdb
Robert <robert.aleks.zuk@gmail.com>
2022-09-26 14:30:34 +0200

    add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./scss/style.scss";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Header from "./components/Header";
 import HamburgerMenu from "./components/HamburgerMenu";
 import Footer from "./components/Footer";
@@ -8,12 +8,12 @@ import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <HamburgerMenu />
       <AnimatedRoutes />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
